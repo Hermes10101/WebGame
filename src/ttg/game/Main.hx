757c0ce5b56d6ff25159e0ee40714a8da814a354
Thaddeus <technicaltad@gmail.com>
2016-06-08 15:19:00 +0200

    Add files

---
+++ src/ttg/game/Main.hx
@@ -0,0 +1,19 @@
+package ttg.game;
+
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author Tad
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+	}
+
+}
